d4c235bb2362f1845fdb0c6e2a33049519937449
bachhungcb <bachhungcb@gmail.com>
2023-10-27 18:37:15 +0700

    Update single_linked_list.c

--- single_linked_list.c
+++ single_linked_list.c
@@ -11,3 +11,17 @@ Node*makeNode(int v){//allocate memory for a new node
     p->value = v; p->next = NULL;
 }
 
+Node* insertLast(Node* h, int v){ //Node *h is the pointer point to the header of the linked list
+    Node* p = h;
+    if(h == NULL)
+        return makeNode(v);
+    
+    //general case
+    while (p->next != NULL)
+        p = p->next;
+
+    Node* q = makeNode(v);
+    p->next = q;
+    return h;
+}
+
