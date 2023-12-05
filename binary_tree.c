448fe2c5d50bb9ed3817cec01d663a103d83b615
bachhungcb <bachhungcb@gmail.com>
2023-12-05 15:13:13 +0700

    update binary tree

--- binary_tree.c
+++ binary_tree.c
@@ -30,14 +30,14 @@ Node* find(Node* r, char *name){
 }
 
 Node* addLast(Node *p, char *name){
-    if(p==NULL) makeNode(name);
+    if(p==NULL) return makeNode(name);
     p->rightSibling = addLast(p->rightSibling, name);
     return p;
 }
 
 void addChild(char *name, char *child){
     Node* r = find(root, name);
-    if(r = NULL) return;
+    if(r == NULL) return;
     r->leftMostChild = addLast(r->leftMostChild,child);
 }
 
@@ -67,7 +67,9 @@ void processFind(){
 }
 
 void processFindChild(){
-    char name[256]; scanf("%s", name);
+    char name[256];
+    printf("Nhap vao ten can tim kiem: ");
+    scanf("%s->", name);
     Node *p = find(root, name);
     if(p == NULL) printf("Khong tim thay %s", name);
     else{
@@ -95,7 +97,7 @@ int height(Node* p){
 
 void processHeight(){
     char name[256];
-    scanf("%s", name);
+    printf("Nhap vao ten can tim:");scanf("%s", name);
     Node *q = find(root, name);
     if(q == NULL) printf("Khong tim thay ten %s", name);
     else{
@@ -115,22 +117,33 @@ int count(Node* r){
 }
 
 void processCount(){
-    pritnf("So node cua cay la: %d\n", count(root));
+    printf("So node cua cay la: %d\n", count(root));
 }
 
-void freeTree(Node* r){
-    if(r == NULL)return;
-    Node *q = r->leftMostChild;
-    while(q != NULL){
-        Node *sp = q ->rightSibling;
-        free(q);
+void freeTree(Node* r) {
+    if (r == NULL) return;
+
+    Node* q = r->leftMostChild;
+    while (q != NULL) {
+        Node* sp = q->rightSibling;
+        freeTree(q);  
         q = sp;
     }
-    printf("Da giai phong node: %s\n", r->name); free(r);
-    r = NULL;
+
+    printf("Da giai phong node: %s\n", r->name);
+    free(r);
 }
 
-
 int main(){
+    root == NULL;
+    root = addLast(root, "a");
+    addChild("a","b");
+    addChild("a","d");
+    addChild("b", "c");
+    printTree(root);
+    processCount();
+    processFindChild();
+    processHeight();
+    freeTree(root);
     return 0;
 }
